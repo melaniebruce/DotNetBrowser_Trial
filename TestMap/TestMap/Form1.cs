bad40690457268c07304734e62c7cbf2b830eece
melaniebruce <43041442+melaniebruce@users.noreply.github.com>
2018-09-19 14:25:12 -0400

    test app for map integration

---
+++ TestMap/TestMap/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser;
+using DotNetBrowser.WinForms;
+
+namespace TestMap
+{
+    public partial class Form1 : Form
+    {
+        private WinFormsBrowserView browserView;
+        public Form1()
+        {
+            InitializeComponent();
+            browserView = new WinFormsBrowserView() { Dock = DockStyle.Fill };
+            Controls.Add((Control)browserView);
+            //local 
+            //browserView.Browser.LoadURL("C:\\users\\mbruce\\desktop\\ESRI_Leaflet.html");
+            //from web
+            browserView.Browser.LoadURL("https://developers.arcgis.com/javascript/latest/sample-code/intro-sceneview/live/index.html");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
